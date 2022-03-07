ec187252399318eb7448beae4a85e341f7aac8c9
attapun-an <attapunanivat@gmail.com>
2022-03-07 13:59:19 +0000

    pun updated the code to allow for videos over 10 hours

--- metadata_extractor.R
+++ metadata_extractor.R
@@ -4,9 +4,11 @@ edit_index <- read.csv(file.choose())
 
 clean <- edit_index %>% 
   dplyr::select(Source.In, Notes) %>% 
-  dplyr::mutate(Hour = as.character(as.integer(substr(Source.In, 2, 2))-1),
+  dplyr::mutate(Hour = as.character(as.integer(substr(Source.In, 1, 2))-1),
     MinSec = substr(Source.In, 4, 8)) %>% 
   dplyr::transmute(Mark = paste(Hour,MinSec, sep = ":"),
                    Note = Notes)
 
 write.csv(clean, file.choose())
+
+
