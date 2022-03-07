667085fbfacd5c47d18d974f4f1aa472b521cdf0
attapun-an <attapunanivat@gmail.com>
2022-03-07 11:49:50 +0000

    Initial commit

---
+++ metadata_extractor.R
@@ -0,0 +1,12 @@
+library(dplyr)
+
+edit_index <- read.csv(file.choose()) 
+
+clean <- edit_index %>% 
+  dplyr::select(Source.In, Notes) %>% 
+  dplyr::mutate(Hour = as.character(as.integer(substr(Source.In, 2, 2))-1),
+    MinSec = substr(Source.In, 4, 8)) %>% 
+  dplyr::transmute(Mark = paste(Hour,MinSec, sep = ":"),
+                   Note = Notes)
+
+write.csv(clean, file.choose())
